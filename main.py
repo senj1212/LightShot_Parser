27b365614881667867f46483fb4d5c944d58e439
senj1212 <52883540+senj1212@users.noreply.github.com>
2020-04-14 13:35:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import func
+import threading
+
+ip = '163.172.182.164'
+port = '3128'
+utf = '{0}:{1}'.format(ip, port)
+proxies = {
+'http' : 'http://'+utf,
+'https' : 'https://'+utf
+}
+def scren():
+    flow = func.Img()
+    while True:
+        try:
+            page_r, url_two = flow.go_to_url(proxies)
+            if 'has banned your IP' in page_r:
+                print('Ip в бане.')
+            else:
+                page = flow.find_img(page_r)
+                if page is not None:
+                    flow.download_and_save(page, url_two)
+        except OSError:
+            print('Разрыв сети...(Переподключение)')
+
+
+menu = [scren]
+
+act = int(input('0: Парсер скриншотов;\n'))
+enter_quantity = int(input('Количество потоков(скорость поиска и нагрузка на пк, рекомендуеться 5): '))
+for i in range(0, enter_quantity):
+    threading.Thread(target=menu[act]).start()
